349403b3e97f3bb9f8d83f4faafc4f5f36cddc7b
Achintya Nath <achintya_n@cs.iitr.ac.in>
2021-05-16 09:49:42 +0530

    Add complete profile page

---
+++ update2.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form id="form3" action="">
+    <table>
+   <tr>
+                <div>
+                 
+                   <td><label for="college">College</label></td>
+                    <td><input type="text" id="college" name="college">
+                    
+                    </td>
+               </div>
+     </tr>
+     <tr>
+            <div>
+            <td>
+                <label for="school">School</label>
+                </td>
+                <td>
+                <input type="text" id="school" name="school">
+                
+                  
+                  </td>
+            </div>
+    </tr>
+    <tr>
+            <div>
+            <td>
+                <label for="birth">Birth Place</label>
+                </td>
+                <td>
+                <input type="text" id="birth" name="birth">
+                
+                  
+                  </td>
+            </div>
+    </tr>
+   <tr>
+    <div>
+    <td>
+                <button type="submit" form="form3" value="Submit" id ="button" style>Update</button>
+</td>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+    </table>
+    </form>
+
+
+    
+    </body>
+</html>
